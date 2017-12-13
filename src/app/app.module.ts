0cb01a76fb5bba7b459977da27419a2cc9fc7ce6
vidhyakrish <vidhyaakrish@gmail.com>
2017-12-13 14:46:39 +0000

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { PriceRequestComponent } from './price-request/price-request.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PriceRequestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
